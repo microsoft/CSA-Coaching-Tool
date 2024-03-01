8978a6d9fcb22dd0f26e9d4b3ad5791ad8239ce3
cajetzer <64230564+cajetzer@users.noreply.github.com>
2024-03-01 15:53:37 -0500

    opp filter fix, admin app changes

--- SolutionManagement.ps1
+++ SolutionManagement.ps1
@@ -15,8 +15,8 @@ pac auth select --index 4
 # Set these before running the script
 
 $solutionDir = ".\Solutions\"
-$solutionVersion = "2.0.0.0"
-$newSolutionVersion = "2.0.0.1"
+$solutionVersion = "2.0.0.1"
+# $newSolutionVersion = "2.0.0.2"
 # $solutionName = "HackTrackerCore"
 # $solutionName = "HackTrackerAdminApp"
 # $solutionName = "HackTrackerSecurityRoles"
@@ -36,12 +36,12 @@ pac solution create-settings -z ${solutionDir}${solutionName}-${solutionVersion}
 # Set these before running the script
 
 $solutionDir = ".\Solutions\"
-$solutionVersion = "2.0.0.0"
-# $solutionName = "HackTrackerCore"
+$solutionVersion = "2.0.0.1"
+$solutionName = "HackTrackerCore"
 # $solutionName = "HackTrackerAdminApp"
 # $solutionName = "HackTrackerSecurityRoles"
 # $solutionName = "HackTrackerFlows"
-$solutionName = "HackTrackerGuidedApps"
+# $solutionName = "HackTrackerGuidedApps"
 
 # $settingsName = "EnvSettings.json"
 
@@ -55,7 +55,7 @@ pac solution import --path ${solutionDir}${solutionName}-${solutionVersion}_mana
 # Set these before running script
 
 $solutionDir = ".\Solutions\"
-$solutionVersion = "2.0.0.0"
+$solutionVersion = "2.0.0.1"
 # $solutionName = "HackTrackerCore"
 # $solutionName = "HackTrackerAdminApp"
 # $solutionName = "HackTrackerSecurityRoles"
